9bdabb36e252df31de151fde83ce11bb5a4a5027
greta <hdouin@free.fr>
2023-12-08 10:50:51 +0100

    =Update Harry Potter

--- harrypotter/src/Components/Navigation/Navigation.js
+++ harrypotter/src/Components/Navigation/Navigation.js
@@ -1,17 +1,29 @@
 import './navigation.css';
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-export const Navigation = () => {
+export const Navigation = (props) => {
+    const [slidedown,setSlidedown] = useState('slidedown hide');
     const location = useLocation()
     let url = location.pathname.toLowerCase().replace('/', '');
+    let width = window.innerWidth;
+    console.log(width);
     return (
         <div className="nav">
-            <Link className="link" to="/">Home</Link>
-            <Link className="link" to="Gryffindor">Gryffindor</Link>
-            <Link className="link" to="Hufflepuff">Hufflepuff</Link>
-            <Link className="link" to="Ravenclaw">Ravenclaw</Link>
-            <Link className="link" to="Slytherin">Slytherin</Link>
+            <div className="slider" onClick={()=>{
+                slidedown == 'slidedown hide' ? setSlidedown('slidedown show') : setSlidedown('slidedown hide');
+            }}><i className="fa fa-bars"></i></div>
+            <Link className="link nav-home" to="/" onClick={()=>{
+                width < 650 ? setSlidedown('slidedown hide') : setSlidedown('slidedown show');
+            }}><i className="fa fa-home"></i></Link>
+            <div className={slidedown}>
+                {props.menus.map((item, i) => {
+                    const active = item.toLowerCase() === url ? 'link active' : 'link';
+                    return (
+                        <Link className={active} to={item}>{item}</Link>
+                    )
+                })}
+            </div>
         </div>
     );
 }
